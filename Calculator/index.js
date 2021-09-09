465e57887273d2c63fb66872cf7bdc31dcbb8e2a
Ankit Raj <ankitraj.raj123@gmail.com>
2021-09-09 19:24:06 +0530

    calculator project

---
+++ Calculator/index.js
@@ -0,0 +1,52 @@
+function one(){
+    let dis = document.getElementById("disp").value += "1"
+}
+function two(){
+    let dis = document.getElementById("disp").value += "2"
+}
+function three(){
+    let dis = document.getElementById("disp").value += "3"
+}
+function four(){
+    let dis = document.getElementById("disp").value += "4"
+}
+function five(){
+    let dis = document.getElementById("disp").value += "5"
+}
+function six(){
+    let dis = document.getElementById("disp").value += "6"
+}
+function seven(){
+    let dis = document.getElementById("disp").value += "7"
+}
+function eight(){
+    let dis = document.getElementById("disp").value += "8"
+}
+function nine(){
+    let dis = document.getElementById("disp").value += "9"
+}
+function zero(){
+    let dis = document.getElementById("disp").value += "0"
+}
+function dot(){
+    let dis = document.getElementById("disp").value += "."
+}
+function add(){
+    let dis = document.getElementById("disp").value += "+"
+}
+function sub(){
+    let dis = document.getElementById("disp").value += "-"
+}
+function multi(){
+    let dis = document.getElementById("disp").value += "*"
+}
+function divide(){
+    let dis = document.getElementById("disp").value += "/"
+}
+function result(){
+    let dis = document.getElementById("disp").value = eval(document.getElementById("disp").value)
+}
+function clr(){
+    let dis = document.getElementById("disp").value = ""
+}
+
